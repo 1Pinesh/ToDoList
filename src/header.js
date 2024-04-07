05f188db604f6133d51843614977e4bdf76e678f
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2024-04-07 14:04:26 +0100

    the header function has beeen created now i will turn it into a class constructor

--- src/header.js
+++ src/header.js
@@ -1,33 +1,37 @@
 // HeaderContainer
-const headerContainer = document.createElement("div");
-headerContainer.classList.add("headerCont");
+const header = () => {
+  const headerContainer = document.createElement("div");
+  headerContainer.classList.add("headerCon");
 
-// Logo Container
-const logoContainer = document.createElement("div");
-logoContainer.classList.add("logoContainer");
+  // Logo Container
+  const logoContainer = document.createElement("div");
+  logoContainer.classList.add("logoContainer");
 
-// Logo
-const logo = document.createElement("h1");
-logo.classList.add("logo");
-logo.textContent = "To-Do-List";
+  // Logo
+  const logo = document.createElement("h1");
+  logo.classList.add("logo");
+  logo.textContent = "To-Do-List";
 
-// subHeading Container
-const subHeadingContainer = document.createElement("div");
+  // subHeading Container
+  const subHeadingContainer = document.createElement("div");
+  subHeadingContainer.classList.add(subHeadingCon);
+  // SubHeading
+  const a = document.createElement("a");
+  a.textContent = "About Us";
 
-// SubHeading
-const a = document.createElement("a");
-a.textContent = "About Us";
+  const h = document.createElement("a");
+  h.textContent = "help";
 
-const h = document.createElement("a");
-h.textContent = "help";
+  subHeadingContainer.appendChild(a);
+  subHeadingContainer.appendChild(h);
 
-subHeadingContainer.appendChild(a);
-subHeadingContainer.appendChild(h);
+  // adding logo to logo contrainer
+  logoContainer.appendChild(logo);
 
-// adding logo to logo contrainer
-logoContainer.appendChild(logo);
+  headerContainer.appendChild(logoContainer);
+  headerContainer.appendChild(subHeadingContainer);
 
-// headerContainer.appendChild(logoContainer);
-headerContainer.appendChild(subHeadingContainer);
+  return headerContainer;
+};
 
-document.body.appendChild(headerContainer);
+document.body.appendChild(header());
