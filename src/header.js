66d7a3c75ad270caa4ca5bcad3ec4dfde13b9711
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2024-04-07 14:07:37 +0100

    refactor my code to be what the assignment has asked for

--- src/header.js
+++ src/header.js
@@ -1,37 +1,47 @@
-// HeaderContainer
-const header = () => {
-  const headerContainer = document.createElement("div");
-  headerContainer.classList.add("headerCon");
+class Header {
+  constructor() {
+    // HeaderContainer
+    const header = () => {
+      const headerContainer = document.createElement("div");
+      headerContainer.classList.add("headerCon");
 
-  // Logo Container
-  const logoContainer = document.createElement("div");
-  logoContainer.classList.add("logoContainer");
+      // Logo Container
+      const logoContainer = document.createElement("div");
+      logoContainer.classList.add("logoContainer");
 
-  // Logo
-  const logo = document.createElement("h1");
-  logo.classList.add("logo");
-  logo.textContent = "To-Do-List";
+      // Logo
+      const logo = document.createElement("h1");
+      logo.classList.add("logo");
+      logo.textContent = "To-Do-List";
 
-  // subHeading Container
-  const subHeadingContainer = document.createElement("div");
-  subHeadingContainer.classList.add(subHeadingCon);
-  // SubHeading
-  const a = document.createElement("a");
-  a.textContent = "About Us";
+      // subHeading Container
+      const subHeadingContainer = document.createElement("div");
+      subHeadingContainer.classList.add(subHeadingCon);
+      // SubHeading
+      const a = document.createElement("a");
+      a.textContent = "About Us";
 
-  const h = document.createElement("a");
-  h.textContent = "help";
+      const h = document.createElement("a");
+      h.textContent = "help";
 
-  subHeadingContainer.appendChild(a);
-  subHeadingContainer.appendChild(h);
+      subHeadingContainer.appendChild(a);
+      subHeadingContainer.appendChild(h);
 
-  // adding logo to logo contrainer
-  logoContainer.appendChild(logo);
+      // adding logo to logo contrainer
+      logoContainer.appendChild(logo);
 
-  headerContainer.appendChild(logoContainer);
-  headerContainer.appendChild(subHeadingContainer);
+      headerContainer.appendChild(logoContainer);
+      headerContainer.appendChild(subHeadingContainer);
 
-  return headerContainer;
-};
+      return headerContainer;
+    };
+  }
+  getHeaderElement() {
+    return this.headerContainer;
+  }
+}
+
+// create the header obj
+const headerObj = new Header();
 
 document.body.appendChild(header());
