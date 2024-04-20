279c10501db13c8a7a8ebd085e2a26e2c966395b
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2024-04-20 18:16:14 +0100

    had to remove menu.js as this will be done once i have create project and toDo list

--- src/mainMenu.js
+++
@@ -1,9 +0,0 @@
-class Menubar {
-  constructor() {
-    // menuContainer
-    this.menuContainer = document.createElement("div");
-    this.menuContainer.classList.add("menuCon");
-
-    //Project Container
-  }
-}
